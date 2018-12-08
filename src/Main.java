0c403b70c4a65458f2e5556cfa0992c70770b905
EdoardoBaral <45716301+EdoardoBaral@users.noreply.github.com>
2018-12-08 18:04:12 +0100

    Creazione classe Main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Persona p = new Persona();
+        p.setNome("Edoardo");
+        p.setCognome("Baral");
+        p.setEta(27);
+
+        System.out.println(p.toString());
+    }
+}
